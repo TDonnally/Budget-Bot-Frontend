badf4bddf386da3c3a3862ba8ecb0459c57d210b
unknown <tristandonnally@gmail.com>
2023-08-29 16:08:06 -0400

    updates to chart

--- budgetai/src/components/NetWorthChart.js
+++ budgetai/src/components/NetWorthChart.js
@@ -1,12 +1,105 @@
-import React, { useState } from "react";
-import Chart from 'chart.js/auto';
+import React, { useRef, useEffect } from "react";
+import 'chart.js/auto'
+import { Line } from 'react-chartjs-2';
 
 function NetWorthChart() {
+    const chartRef = useRef(null);
+    
+    const data = {
+        labels: ["Jun", "Jul", "Aug", "Sep", "Oct", "Nov"],
+        datasets: [
+            {
+                id: 1,
+                fill: "origin",
+                backgroundColor: (context) => {
+                    const chart = context.chart;
+                    const { ctx, chartArea } = chart;
+                    if (!chartArea) {
+                        return null;
+                    }
+                    const gradient = ctx.createLinearGradient(
+                        chartArea.left,
+                        chartArea.bottom,
+                        chartArea.left,
+                        chartArea.top
+                    );
+                    gradient.addColorStop(0, "rgba(75, 192, 192, 0)");
+                    gradient.addColorStop(1, "rgba(75, 192, 192, 1)");
+                    return gradient;
+                },
+                borderColor: "rgba(75, 192, 192, 0)",
+                label: "",
+                data: [65, 59, 67, 52, 48, 55],
+                pointRadius: [0, 0, 0, 0, 0, 0]
+            },
+            {
+                id: 2,
+                fill: "-1",
+                backgroundColor: "rgb(255, 255, 255)",
+                borderColor: "rgb(75, 192, 192)",
+                borderWidth: 4,
+                label: "",
+                data: [65, 59, 67, 52, 48, 55],
+                pointRadius: [0, 0, 0, 0, 0, 5], // Set pointRadius to 5 for the last data point
+            },
+        ],
+    };
+
+    const options = {
+        /*onHover: function (e, item) {
+            const chart = chartRef.current
+            //console.log(e);
+            console.log("MouseX: ", e.x, "MouseY:", e.y);
+            console.log(chart);
+        },*/
+        interaction: {
+            mode: 'index',
+            intersect: false,
+          },
+        animation: {
+            duration: 0,
+            easing: "easeInOutQuad"
+        },
+        scales: {
+            y: {
+                display: false,
+                grid: {
+                    display: false,
+                },
+            },
+            x: {
+                display: false,
+                grid: {
+                    display: false,
+                },
+            },
+        },
+        plugins: {
+            title: {
+                display: false,
+            },
+            legend: {
+                display: false,
+            },
+            filler: {
+                propagate: false,
+            },
+            elements: {
+                point: {
+                    radius: 0,
+                },
+            },
+            tooltip: {
+                enabled: false,
+                position: 'nearest'
+            },
+        },
+    };
     
 
     return (
-        <div>
-            Chart
+        <div className="chart-container">
+            <Line ref={chartRef}data={data} options={options} />
         </div>
     );
 }
