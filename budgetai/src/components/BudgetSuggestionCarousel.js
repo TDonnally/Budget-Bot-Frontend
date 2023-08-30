0fc6ac6a040525b2e3dbd706988505d653d00da6
unknown <tristandonnally@gmail.com>
2023-08-30 13:49:20 -0400

    updates to slider

--- budgetai/src/components/BudgetSuggestionCarousel.js
+++ budgetai/src/components/BudgetSuggestionCarousel.js
@@ -4,7 +4,7 @@ import "@glidejs/glide/dist/css/glide.core.min.css";
 
 
 const sliderConfiguration= {
-    autoplay: 1000,
+    autoplay: 4000,
   };
 
 function BudgetSuggestionCarousel({ responseData }) {
@@ -17,38 +17,41 @@ const mainGlide = new Glide(".main__glide", sliderConfiguration); // default opt
   }, [mainGlide]);
 
 return (
-  <div className="main__glide">
-    <div className="glide__track" data-glide-el="track">
-        <ul className="glide__slides">
-          <li className="glide__slide">
-             1
-         </li>
-         <li className="glide__slide">
-             2
-         </li>
-         <li className="glide__slide">
-             3
-         </li>
-         <li className="glide__slide">
-             4
-         </li>
-      </ul>
-  </div>
-  <div className="glide__arrows" data-glide-el="controls">
-            <button
-              className="glide__arrow glide__arrow--left"
-              data-glide-dir="<"
-            >
-              prev
-            </button>
-            <button
-              className="glide__arrow glide__arrow--right"
-              data-glide-dir=">"
-            >
-              next
-            </button>
-          </div>
-</div>
+    <div className = "slider-container">
+        <h3>Spending Suggestions</h3>
+        <div className="main__glide">
+            <div className="glide__track" data-glide-el="track">
+                <ul className="glide__slides">
+                <li className="glide__slide">
+                    <p>Your dining out expenses for this month totaled $600. Comparing this amount to your monthly income, it appears to be 15.6% higher than the recommended allocation for dining expenses. While indulging in dining experiences is enjoyable, it might be worth considering if there are ways to manage this expense in line with your financial goals.</p>
+                </li>
+                <li className="glide__slide">
+                    <p>Your recent travel expenditures amounted to $800. When measured against your current income, this figure is 18.9% higher than the typically advised budget for travel. While exploring new destinations and experiences is exciting, it's a good opportunity to assess if there are potential adjustments that can help align your travel spending with your overall financial plan.</p>
+                </li>
+                <li className="glide__slide">
+                    <p>Your clothing and accessories purchases for this period reached $300. Evaluating this spending against your income, it's evident that you've exceeded the recommended budget for such expenses by 10.8%. Keeping your style up-to-date can be enjoyable, but it's worth considering how you might strike a balance between expressing your personal fashion preferences and maintaining a prudent financial approach.</p>
+                </li>
+                <li className="glide__slide">
+                    <p>Your recent entertainment and hobbies spending amounts to $250. When compared with your monthly earnings, this expenditure surpasses the advised allocation by 8.2%. While pursuing leisure activities and hobbies is a valuable way to unwind, it's wise to assess how you can maintain your interests while also ensuring your financial wellness remains a priority.</p>
+                </li>
+            </ul>
+        </div>
+        <div className="glide__arrows" data-glide-el="controls">
+                    <button
+                    className="glide__arrow glide__arrow--left"
+                    data-glide-dir="<"
+                    >
+                    prev
+                    </button>
+                    <button
+                    className="glide__arrow glide__arrow--right"
+                    data-glide-dir=">"
+                    >
+                    next
+                    </button>
+                </div>
+        </div>
+    </div>
 )
     
   }
