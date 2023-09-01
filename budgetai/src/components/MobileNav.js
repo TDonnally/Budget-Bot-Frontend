612b8afe846cbaa88a48f89c892db0defe1459cb
unknown <tristandonnally@gmail.com>
2023-08-31 23:56:33 -0400

    Added mobile menu animations and Styles

--- budgetai/src/components/MobileNav.js
+++ budgetai/src/components/MobileNav.js
@@ -1,15 +1,25 @@
 import React, { useState } from "react";
+import { FaHouse, FaChartSimple, FaUser } from "react-icons/fa6";
 
 function MobileNav() {
    
     return (
         <div className="mobile-nav-container">
-            <label for="check">
-                <input type="checkbox" id="check"/> 
-                <span></span>
-                <span></span>
-                <span></span>
-            </label>
+            <div className="mobile-nav-check">
+                <label htmlFor="check">
+                    <input type="checkbox" id="check"/> 
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <div id = "icons-cover"></div>
+                    <div className="mobile-nav">
+                        <FaHouse/>
+                        <FaChartSimple/>
+                        <FaUser/>
+                    </div>
+                </label>
+            </div>
+            
         </div>
     );
     
