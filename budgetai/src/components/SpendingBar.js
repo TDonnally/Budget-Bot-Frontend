a66476cc3a8a550aebac2030dd4fb5fb57fe4bde
unknown <tristandonnally@gmail.com>
2023-09-04 20:33:51 -0400

    removed grid line from spending chart

--- budgetai/src/components/SpendingBar.js
+++ budgetai/src/components/SpendingBar.js
@@ -23,25 +23,44 @@ function SpendingBar() {
             {
                 data: [12, 19, 3, 5, 2, 3],
                 backgroundColor: ['rgba(255, 99, 132, 0.5)', 'rgba(54, 162, 235, 0.5)', 'rgba(255, 206, 86, 0.5)', 'rgba(75, 192, 192, 0.5)', 'rgba(153, 102, 255, 0.5)', 'rgba(255, 159, 64, 0.5)'],
-
             },
         ],
     };
 
     const options = {
         indexAxis: 'y',
+        
         elements: {
             bar: {
             },
         },
+        scales: {
+            y: {
+                grid: {
+                    display: false,
+                },
+                ticks: {
+                    color: 'white',
+                    weight: 'bold',
+                }
+            },
+            x: {
+                grid: {
+                    display: false,
+                },
+                ticks: {
+                  color: 'white',
+                  weight: 'bold'
+                }
+              },
+          },
         responsive: true,
         plugins: {
+            
             legend: {
                 display: false,
                 position: 'bottom',
-                labels: {
-                    usePointStyle: true, // To show individual colors for legend items
-                },
+                
             },
             title: {
                 display: false,
