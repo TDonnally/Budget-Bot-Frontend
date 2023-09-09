e8f6fe078b816b0eb8b103f9211db37b8350da55
unknown <tristandonnally@gmail.com>
2023-09-08 23:12:12 -0400

    added settings menu styles

--- budgetai/src/components/views/Settings.js
+++ budgetai/src/components/views/Settings.js
@@ -2,64 +2,100 @@ import React, { useState, useEffect } from "react";
 import axios from "axios"; 
 import { getCookie, setCookie } from "../../scripts/cookies";
 import PlaidLink from "../PlaidLink";
+import { FaUserPen, FaBuildingColumns, FaRegCreditCard, FaMoneyBills } from "react-icons/fa6";
 
 function Settings({ responseData, parentLocation }) {
-  const [activeButton, setActiveButton] = useState(null);
+    const [activeButton, setActiveButton] = useState('.account');
 
-  useEffect(() => {
-    // Add click event listeners to all option elements
-    const optionButtons = document.querySelectorAll('.option-button');
-    optionButtons.forEach(function (button) {
-      button.addEventListener('click', handleOptionClick);
-    });
-  }, []);
+    useEffect(() => {
+        //open account details tab
+        const initOpenElement = document.querySelector(activeButton)
+        initOpenElement.classList.remove('hide');
+        // Add click event listeners to all option elements
+        const optionsContainer = document.querySelector('.options');
+        optionsContainer.addEventListener('click', handleOptionClick);
 
-  // Function to handle the click event when an option is selected
-  function handleOptionClick(event) {
-    const selectedOption = event.target;
-    const show = selectedOption.dataset.show;
+        // Cleanup the event listener when the component unmounts
+        return () => {
+            optionsContainer.removeEventListener('click', handleOptionClick);
+        };
+    }, []);
 
-    // Remove the 'hide' class from the element to show
-    const showElement = document.querySelector(show);
-    if (showElement) {
-      showElement.classList.remove('hide');
+    // Function to handle the click event when an option is selected
+    function handleOptionClick(event) {
+        const selectedOption = event.target.closest('.option-button');
+
+        if (!selectedOption) {
+            return; // If no 'option-button' was clicked, do nothing
+          }
+
+        //selectedOption.classList.add("active");
+        const show = selectedOption.dataset.show;
+
+        // Remove the 'hide' class from the element to show
+        const showElement = document.querySelector(show);
+        if (showElement) {
+            showElement.classList.remove('hide');
+        }
+
+        // Add the 'hide' class to all other content divs
+        const allContentDivs = document.querySelectorAll('.option > div');
+        allContentDivs.forEach(function (div) {
+            if (div !== showElement) {
+            div.classList.add('hide');
+            }
+        });
+
+        // Update the active button
+        setActiveButton(selectedOption.dataset.show);
+
+        // Remove the 'selected' class from all other buttons
+        const optionButtons = document.querySelectorAll('.option-button');
+        optionButtons.forEach(function (button) {
+            if (button !== selectedOption) {
+            button.classList.remove('active');
+            }
+        });
     }
-
-    // Add the 'hide' class to all other content divs
-    const allContentDivs = document.querySelectorAll('.my-info-1 > div');
-    allContentDivs.forEach(function (div) {
-      if (div !== showElement) {
-        div.classList.add('hide');
-      }
-    });
-
-    // Update the active button
-    setActiveButton(selectedOption);
-
-    // Remove the 'selected' class from all other buttons
-    const optionButtons = document.querySelectorAll('.option-button');
-    optionButtons.forEach(function (button) {
-      if (button !== selectedOption) {
-        button.classList.remove('selected');
-      }
-    });
-  }
 
   return (
     <div>
-      <div className="options">
-        <div className={`option-button${activeButton === 'Account' ? ' selected' : ''}`} data-show=".account">Account Details</div>
-        <div className={`option-button${activeButton === 'institutions' ? ' selected' : ''}`} data-show=".institutions">Institutions</div>
-        <div className={`option-button${activeButton === 'Payment' ? ' selected' : ''}`} data-show=".payment">Payment Info</div>
-        <div className={`option-button${activeButton === 'Budget' ? ' selected' : ''}`} data-show=".budget">Budget Allocation</div>
-      </div>
+        <h2>Settings</h2>
+        <div className="settings-control-container">
+            <div className="options">
+                <div className={`option-button${activeButton === '.account' ? ' selected' : ''}`} data-show=".account">
+                    <FaUserPen/>
+                    <span>Account Details</span>
+                </div>
+                <div className={`option-button${activeButton === '.institutions' ? ' selected' : ''}`} data-show=".institutions">
+                    <FaBuildingColumns/>
+                    <span>Institutions</span>
+                </div>
+                <div className={`option-button${activeButton === '.payment' ? ' selected' : ''}`} data-show=".payment">
+                    <FaRegCreditCard/>
+                    <span>Payment Info</span>
+                </div>
+                <div className={`option-button${activeButton === '.budget' ? ' selected' : ''}`} data-show=".budget">
+                    <FaMoneyBills/>
+                    <span>Budget Allocation</span>
+                </div>
+            </div>
 
-      <div className="my-info-1">
-        <div className={`account`}>Account</div>
-        <div className={`institutions hide`}>Institutions</div>
-        <div className={`payment hide`}>Payment</div>
-        <div className={`budget hide`}>Budget</div>
-      </div>
+            <div className="option">
+                <div className={`account hide`}>
+                    <span>Account</span>
+                </div>
+                <div className={`institutions hide`}>
+                    <span>Institutions</span>
+                </div>
+                <div className={`payment hide`}>
+                    <span>Payment</span>
+                </div>
+                <div className={`budget hide`}>
+                    <span>Budget</span>
+                </div>
+            </div>
+        </div>     
     </div>
   );
 }
