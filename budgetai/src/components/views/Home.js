8a2014b3d376d63505fd13cf1e0a1bcdea81ac1b
unknown <tristandonnally@gmail.com>
2023-09-20 22:48:57 -0400

    adjusted props calls so arrays are accoutned for in Home.js

--- budgetai/src/components/views/Home.js
+++ budgetai/src/components/views/Home.js
@@ -9,36 +9,42 @@ import PlaidLink from "../PlaidLink";
 
 
 function Home(props) {
-  const accountsArray = props.responseData.accountsData.accounts;
-  const transactionsArray = props.responseData.transactionsData.transactions;
+  const accountsArray = props.responseData.accountsData;
+  const transactionsArray = props.responseData.transactionsData;
   const today = new Date();
 
   const [netWorth, setNetWorth ] = useState(0);
+  const[accountsArrayCleaned, setAccountsArrayCleaned] = useState([]);
   useEffect(() => { 
     let month = today.getMonth() + 1;
     let currentWorth = 0;
     let newWorth = 0;
+    let accountsArrayHolder = [];
     let updatedChartData = [];
     //Init current net worth
     for(var i = 0; i<accountsArray.length; i++){
-      currentWorth += accountsArray[i].balances.current;
-      newWorth = currentWorth;
+      for(var j = 0; j < accountsArray[i].accounts.length; j++){
+        accountsArrayHolder.push(accountsArray[i].accounts[j]);
+        currentWorth += accountsArray[i].accounts[j].balances.current;
+        newWorth = currentWorth;
+      }
     }
     setNetWorth(currentWorth);
+    setAccountsArrayCleaned(accountsArrayHolder);
 
     //Init past net worths and store them in an array
     
   }, [])
 
-  console.log(netWorth);
 
   return (
       <div>
         <p style = {{fontSize: '0px'}}>Home</p>
         <NetWorthChart netWorth = {netWorth}/>
         <div className="chart-cover">
-          <AccountSummaries accountsArray = {accountsArray} />
+          <AccountSummaries accountsArray = {accountsArrayCleaned} />
           <BudgetSuggestionCarousel/>
+          <PlaidLink user = {getCookie('email')}/>
           <SpendingBar  transactionsArray = {transactionsArray} />
         </div>
         {/*
